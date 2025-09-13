9e7aaa77b8cf77b496fcaa0ea356d200a8421b78
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2025-09-12 23:50:03 -0300

    13 - Estruturando como os dados serão analizados

--- app/models/dados_boletim_model.py
+++ app/models/dados_boletim_model.py
@@ -1,3 +1,9 @@
+from models.estoque_model import EstoqueModel
+from models.faturamento_model import FaturamentoModel
+
+
+
+class DadosBoletimModel:pass
 class DadosBoletimModel:
     def __init__(self,
             qtd_estoque_consumido_ton: int,
@@ -15,3 +21,36 @@ class DadosBoletimModel:
         self.skus_alto_giro_sem_estoque = skus_alto_giro_sem_estoque
         self.itens_a_repor = itens_a_repor
         self.risco_desabastecimento_sku1 = risco_desabastecimento_sku1
+    
+    def from_raw_data(dados_estoque: list[EstoqueModel], dados_faturamento: list[FaturamentoModel]) -> DadosBoletimModel:
+        #TODO
+        # Qual a quantidade de estoque consumido em toneladas (es_totalestoque) das últimas 52 semanas?
+        # somar os valores da coluna es_totalestoque de dados_estoque
+
+        # Qual a frequência de compra (meses com zs_peso_liquido >0) das últimas 52 semanas?
+        # contar quantos dos meses nas últimas 52 semanas possuíram o zs_peso_liquido total > 0
+
+        # O valor do aging de estoque (idade em semanas)?
+        # valor médio da coluna dias_em_estoque de dados_estoque
+
+        # Quantos clientes consomem o material SKU_1
+        # contar quantos clientes distintos (cod_cliente em dados_estoque) consomem SKU_1
+
+        # Quais SKUs de alto giro e alta frequencia que estão sem estoque?
+        # definir o que é um SKU de alto giro e frequencia (coluna giro_sku_cliente em dados_faturamento)
+
+        # Quais são os itens a serem repostos no estoque?
+        # <SKUs com alto giro e giro_sku_cliente baixo es_totalestoque???>
+
+        # Qual o risco de desabastecimento do SKU_1?
+        # <talvez calcular a quantidade recente está abaixo da média???>
+
+        DadosBoletimModel(
+            qtd_estoque_consumido_ton = ,
+            freq_compra = ,
+            valor_aging = ,
+            qtd_consomem_sku1 = ,
+            skus_alto_giro_sem_estoque = ,
+            itens_a_repor = ,
+            risco_desabastecimento_sku1 = 
+        )
---
+++ app/models/faturamento_model.py
@@ -0,0 +1,29 @@
+class FaturamentoModel:
+    def __init__(self,
+            data: str,
+            cod_cliente: int,
+            lote: str,
+            origem: str,
+            zs_gr_mercad: str,
+            produto: str,
+            cod_produto: str,
+            zs_centro: str,
+            zs_cidade: str,
+            zs_uf: str,
+            zs_peso_liquido: float,
+            giro_sku_cliente: float,
+            SKU: str
+        ):
+        self.data = data
+        self.cod_cliente = cod_cliente
+        self.lote = lote
+        self.origem = origem
+        self.zs_gr_mercad = zs_gr_mercad
+        self.produto = produto
+        self.cod_produto = cod_produto
+        self.zs_centro = zs_centro
+        self.zs_cidade = zs_cidade
+        self.zs_uf = zs_uf
+        self.zs_peso_liquido = zs_peso_liquido
+        self.giro_sku_cliente = giro_sku_cliente
+        self.SKU = SKU
