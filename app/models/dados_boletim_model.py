3c056e19b34b2db61bc58ecb674d2d06f4053016
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2025-09-13 11:46:27 -0300

    13 - Evoluindo na estrutura da criação do boletim

--- app/models/dados_boletim_model.py
+++ app/models/dados_boletim_model.py
@@ -8,15 +8,19 @@ class DadosBoletimModel:
     def __init__(self,
             qtd_estoque_consumido_ton: int,
             freq_compra: int,
-            valor_aging: int,
+            valor_aging_min: int,
+            valor_aging_avg: int,
+            valor_aging_max: int,
             qtd_consomem_sku1: int,
             skus_alto_giro_sem_estoque: list[int],
             itens_a_repor: list[str],
-            risco_desabastecimento_sku1: float
+            risco_desabastecimento_sku1: str
         ):
         self.qtd_estoque_consumido_ton = qtd_estoque_consumido_ton
         self.freq_compra = freq_compra
-        self.valor_aging = valor_aging
+        self.valor_aging_min = valor_aging_min
+        self.valor_aging_avg = valor_aging_avg
+        self.valor_aging_max = valor_aging_max
         self.qtd_consomem_sku1 = qtd_consomem_sku1
         self.skus_alto_giro_sem_estoque = skus_alto_giro_sem_estoque
         self.itens_a_repor = itens_a_repor
@@ -26,6 +30,7 @@ class DadosBoletimModel:
         #TODO
         # Qual a quantidade de estoque consumido em toneladas (es_totalestoque) das últimas 52 semanas?
         # somar os valores da coluna es_totalestoque de dados_estoque
+        qtd_estoque_consumido = sum([dados.es_totalestoque for dados in dados_estoque])
 
         # Qual a frequência de compra (meses com zs_peso_liquido >0) das últimas 52 semanas?
         # contar quantos dos meses nas últimas 52 semanas possuíram o zs_peso_liquido total > 0
@@ -35,17 +40,25 @@ class DadosBoletimModel:
 
         # Quantos clientes consomem o material SKU_1
         # contar quantos clientes distintos (cod_cliente em dados_estoque) consomem SKU_1
+        qtd_consomem_sku1 = len(set([dados.cod_cliente for dados in dados_estoque if dados.SKU == "SKU_1"]))
 
         # Quais SKUs de alto giro e alta frequencia que estão sem estoque?
         # definir o que é um SKU de alto giro e frequencia (coluna giro_sku_cliente em dados_faturamento)
 
         # Quais são os itens a serem repostos no estoque?
-        # <SKUs com alto giro e giro_sku_cliente baixo es_totalestoque???>
+        # <SKUs com alto giro_sku_cliente e baixo es_totalestoque???>
 
         # Qual o risco de desabastecimento do SKU_1?
         # <talvez calcular a quantidade recente está abaixo da média???>
+        risco_desabastecimento_sku1 = "desconhecido"
+        if atual < media:
+            risco_desabastecimento_sku1 = "alto, pois o estoque atual está abaixo da média das últimas 52 semanas"
+        elif atual == media:
+            risco_desabastecimento_sku1 = "baixo, pois o estoque atual está próximo da média das últimas 52 semanas"
+        elif atual > media:
+            risco_desabastecimento_sku1 = "muito baixo, pois o estoque atual está acima da média das últimas 52 semanas"
 
-        DadosBoletimModel(
+        return DadosBoletimModel(
             qtd_estoque_consumido_ton = ,
             freq_compra = ,
             valor_aging = ,
@@ -54,3 +67,6 @@ class DadosBoletimModel:
             itens_a_repor = ,
             risco_desabastecimento_sku1 = 
         )
+
+    def get_report_str(self) -> str:
+        return f"A quantidade total de estoque consumido em toneladas nas últimas 52 semanas foi de {self.qtd_estoque_consumido_ton}; A frequência de compra (meses com zs_peso_liquido > 0) nas últimas 52 semanas foi de {self.freq_compra} ou {(52/4) / self.freq_compra}; O valor do aging de estoque (em semanas) mínimo, médio e máximo foram respectivamente {self.valor_aging_min}, {self.valor_aging_avg} e {self.valor_aging_max}; O número de clientes que consomem o SKU_1 é {self.qtd_consomem_sku1}; Os itens com SKU {', '.join(self.skus_alto_giro_sem_estoque)} foram identificados como de alto giro e alta frequência e estão sem estoque; Os itens SKU {', '.join(self.itens_a_repor)} que precisam ser repostos; O risco de desabastecimento do SKU_1 é {self.risco_desabastecimento_sku1}."
--- app/services/boletim_service.py
+++ app/services/boletim_service.py
@@ -25,12 +25,10 @@ class BoletimService:
         self.tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-2b-it")
 
     def gerar_str_boletim(self, dados: DadosBoletimModel) -> str:
-        #TODO
-        # Inserir no prompt os valores reais do boletim
-        prompt = """
+        prompt = f"""
 Escreva um boletim corporativo em português, em parágrafos corridos, usando linguagem formal e profissional. Escreva texto contínuo dividido em parágrafos. Use apenas os dados fornecidos para compor o relatório. Seja mais analítico do que descritivo: conecte as métricas entre si, aponte implicações operacionais e conclua com uma avaliação de risco prática e objetiva sem recomendações. O boletim deve conter de 400 a 1000 palavras.
 
-Pontos obrigatórios a contemplar no texto (sem numerar na saída): A quantidade total de estoque consumido em toneladas nas últimas 52 semanas foi de 35.000; A frequência de compra (meses com zs_peso_liquido > 0) nas últimas 52 semanas foi de 7; O valor do aging de estoque (em semanas) foi de 3; O número de clientes que consomem o SKU_1 é 10; Os SKUs 13, 20 e 28 foram identificados como de alto giro e alta frequência e estão sem estoque; Os itens SKU 5, 10, 13, 20, 25 e 28 que precisam ser repostos; O risco de desabastecimento do SKU_1 é baixo. Integre essas informações em um fluxo narrativo lógico e coeso.
+Pontos obrigatórios a contemplar no texto (sem numerar na saída): {dados.get_report_str()} Integre essas informações em um fluxo narrativo lógico e coeso.
 """
         input_ids = self.tokenizer(prompt, return_tensors="pt")
         input_ids = {k: v.to(self.device) for k, v in input_ids.items()}
