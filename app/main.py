ef2c18223e2b627d0b60cabdf6f2d1b6d916ab68
KatianeFatec <katiane.soares@fatec.sp.gov.br>
2025-09-12 22:11:10 -0300

    fix - Adiciona execução direta do servidor Uvicorn no main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello, Backend em Python!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
