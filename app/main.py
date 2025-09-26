74d422badb6d64002b52ba1c15b8db8ac06d2770
KatianeFatec <katiane.soares@fatec.sp.gov.br>
2025-09-25 23:33:52 -0300

    8 - inclui o user_router na main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from routes.auth import router as auth_router  
+from routes.user import router as user_router
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hello, Backend em Python!"}
+
+app.include_router(auth_router)
+app.include_router(user_router)
 
 if __name__ == "__main__":
     import uvicorn
