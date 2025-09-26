fb762e904f73a1a3606d900f8eabee04f51c97ac
Mariana <mariana.izumiks@gmail.com>
2025-09-25 21:48:05 -0300

    Task-16 Organizando funcoes e alterando para enviar email ao padrao

--- app/db/db_simulado.py
+++ app/db/db_simulado.py
@@ -1,6 +1,6 @@
 # Simulação de banco de usuários
 usuarios = [
-    {"id": 1, "nome": "Maria", "email": "maria@example.com", "recebe_boletim": True, "is_admin": False},
+    {"id": 1, "nome": "Maria", "email": "dsmjavali@gmail.com", "recebe_boletim": True, "is_admin": False},
     {"id": 2, "nome": "João", "email": "joao@example.com", "recebe_boletim": True, "is_admin": True},
     {"id": 3, "nome": "Ana", "email": "ana@example.com", "recebe_boletim": False, "is_admin": False},
 ]
